be3e8d1a54937f3938a27f6b91e42c0749a7c4dc
Shannazy <shaziislam417@gmail.com>
2019-07-23 21:27:44 -0400

    Create a list that hase Tuples of 4. Trying to pull and stuff

--- Java List/src/ToupleList/EntryPoint.java
+++ Java List/src/ToupleList/EntryPoint.java
@@ -1,8 +1,14 @@
 package ToupleList;
 
+import org.javatuples.Quartet;
+
 public class EntryPoint {
 
     public static void main(String[] args) {
+        myList ob = new myList();
+        Quartet<String, String, String,Integer> my4bitInfo = Quartet.with("Doc_name","Doc_id","Revision_id",5);
+        ob.addInfo(my4bitInfo);
+        System.out.println(ob.giveInfo(3).getValue(1));
 
     }
 }
--- Java List/src/ToupleList/myList.java
+++ Java List/src/ToupleList/myList.java
@@ -1,5 +1,27 @@
 package ToupleList;
 
+import org.javatuples.Quartet;
+import org.javatuples.Triplet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class myList {
-    List<Triplet>
+     private List<Quartet> entries = new ArrayList<>();
+
+    public void addInfo(Quartet info){
+        entries.add(info);
+    }
+
+    public Quartet giveInfo(int x){
+        Quartet Temp = null;
+        try {
+             Temp = entries.get(x);
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("Out of bounds my dude. Returning the last element of the List");
+            Temp =entries.get (entries.size()-1);
+        }
+        return Temp ;
+
+    }
 }
